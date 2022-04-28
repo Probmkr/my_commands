1d6dc31823ac5bff42899fa440ea2e0c4d149fc2
Probmkr <Thanatos9911@gmail.com>
2022-04-28 09:51:06 +0900

    再びやる気を取りもどした。 あと、こっから英語になる。

--- analog/src/main.cpp
+++ analog/src/main.cpp
@@ -6,20 +6,8 @@
 #include <fstream>
 #include <sstream>
 #include <queue>
-#include "lib/arg_types.h"
-#include "lib/arg_parse_funcs.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  // キューを初期化する
-  queue<string> args;
-
-  // キューに引数を入れる
-  for(int i = 0; i < argc; i++) {
-    args.push(argv[i]);
-  }
-
-  // 引数を解析する
-  parse_args(argc, args);
 }
